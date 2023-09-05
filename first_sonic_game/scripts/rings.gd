501815e742d1be82979d34c11a6d8f87413bad39
Rafael Rogeres Farias Xenofonte Amorim <119319891+Xenofontex@users.noreply.github.com>
2023-09-05 14:23:58 -0300

    Add files via upload

---
+++ first_sonic_game/scripts/rings.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var ding_sound: AudioStreamPlayer2D = $ring_sound
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	$ring_sound.play() # Replace with function body.
