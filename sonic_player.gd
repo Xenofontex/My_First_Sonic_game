8efc28bdc17a7d2851e2fb9f9be72833c37baf00
Rafael Rogeres Farias Xenofonte Amorim <119319891+Xenofontex@users.noreply.github.com>
2023-09-06 22:18:07 -0300

    motobug added
    first enemy created, movement and sprite animation.

---
+++ sonic_player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $anim
+@onready var jump_sound = $AudioStreamPlayer2D
+
+var look_up = false
+var is_jumping = false
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		_animated_sprite.play("jump")
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+		is_jumping = true
+		
+	elif is_on_floor():
+		is_jumping = false
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+		
+	if direction:
+		velocity.x = direction * SPEED
+		_animated_sprite.scale.x = direction
+		if !is_jumping:
+			_animated_sprite.play("run")
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			_animated_sprite.play("idle")
+	if Input.is_action_pressed("up") and is_on_floor():
+			look_up = true
+			_animated_sprite.play("look_up")
+
+	move_and_slide()
